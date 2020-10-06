bf487a7b3d4d34a7b2f66962ee492edbbb3056b6
031802615林必涵 <948677135@qq.com>
2020-10-06 19:55:15 +0800

    补充分割的程序

---
+++ imageCutTest.py
@@ -0,0 +1,41 @@
+from PIL import Image
+import sys
+import os
+
+def cut_image(image):
+    width,height = image.size
+    print(width)
+    print(height)
+    item_width = int(width/3)
+    box_list = []
+    for i in range(0,3):
+        for j in range(0,3):
+            box = (j * item_width,i * item_width , (j + 1) * item_width,(i + 1) * item_width)
+            print(box)
+            box_list.append(box)
+    image_list = [image.crop(box) for box in box_list]
+    print(image_list)
+    return image_list
+
+def save_images(image_list,savePath = ""):
+    index = 1
+    for image in image_list:
+        Path = savePath.replace(".jpg","")
+        if(not (os.path.exists("./cutPictures/" + Path))):
+            os.mkdir("./cutPictures/" + Path)
+        image.save("./cutPictures/" + Path + "/" + str(index) + savePath)
+        index += 1
+    return
+
+def save_images2(image_list):
+    index = 1
+    for image in image_list:
+        image.save("./getPictures/" + str(index) + ".jpg")
+        index += 1
+
+if __name__ == '__main__':
+    file_path = "imgtest.jpg"
+    image = Image.open(file_path)
+    image_list = cut_image(image)
+    print(image_list)
+    save_images2(image_list)
